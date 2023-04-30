effc994ffe163dc387cf066edea0c5f6395d9d6c
Julien Richard-Foy <julien@richard-foy.fr>
2023-04-30 19:51:29 +0200

    Add more information to meteograms and sounding diagrams
    Also include a button to switch from meteograms to sounding diagrams and vice versa, and a button to show the help.
    
    Fixes #63

--- frontend/src/help/Help.tsx
+++ frontend/src/help/Help.tsx
@@ -24,6 +24,7 @@ export const Help = (props: { domain: Domain }): JSX.Element => {
         'background-color': 'white'
       }}
       onClick={ () => makeVisible(true) }
+      title="Help"
     >
       ?
     </div>;
