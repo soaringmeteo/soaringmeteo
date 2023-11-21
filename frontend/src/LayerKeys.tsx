79b6b0e7d15fe8c448e8b63f4334420e8c4172db
Julien Richard-Foy <julien@richard-foy.fr>
2023-11-21 08:14:01 +0100

    Show the key only if there is a primary layer displayed on the map

--- frontend/src/LayerKeys.tsx
+++ frontend/src/LayerKeys.tsx
@@ -1,20 +1,22 @@
 import {Domain} from "./State";
-import {JSX} from "solid-js";
+import {JSX, Show} from "solid-js";
 
 export const LayerKeys = (props: {
   domain: Domain
 }): JSX.Element => {
   const primaryLayerComponents = () => props.domain.primaryLayerReactiveComponents();
 
-  return <div style={{
-    position: 'absolute',
-    bottom: '3rem',
-    right: '.5rem',
-    'background-color': 'rgba(255, 255,  255, 0.5',
-    'font-size': '11px',
-    'padding': '5px',
-    'text-align': 'center'
-  }}>
-    {primaryLayerComponents().mapKey}
-  </div>
+  return <Show when={ props.domain.state.primaryLayerEnabled }>
+    <div style={{
+      position: 'absolute',
+      bottom: '3rem',
+      right: '.5rem',
+      'background-color': 'rgba(255, 255,  255, 0.5',
+      'font-size': '11px',
+      'padding': '5px',
+      'text-align': 'center'
+    }}>
+      {primaryLayerComponents().mapKey}
+    </div>
+  </Show>
 };
