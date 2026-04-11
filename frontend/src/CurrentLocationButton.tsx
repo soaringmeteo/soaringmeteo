41197338431affd934ea85e28e8b44bc25605f20
Julien Richard-Foy <julien.richard-foy@terasol.ch>
2026-04-11 14:42:13 +0200

    Use a better icon

--- frontend/src/CurrentLocationButton.tsx
+++ frontend/src/CurrentLocationButton.tsx
@@ -21,12 +21,29 @@ export const CurrentLocationButton = (props: { domain: Domain }): JSX.Element =>
             'border': '1px solid lightgray',
             'box-sizing': 'border-box',
             'background-color': 'white',
+            'display': 'flex',
+            'align-items': 'center',
+            'justify-content': 'center',
             on: $ => [$('hover', { 'background-color': 'lightgray' })]
           })}
           onClick={ () => props.domain.centerMapOnClientLocation() }
           title={ m().mapCenterOnMyLocation() }
         >
-          ◎
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="1.5"
+          >
+            <circle cx="12" cy="12" r="6" />
+            <line x1="12" y1="2" x2="12" y2="6" />
+            <line x1="12" y1="18" x2="12" y2="22" />
+            <line x1="2" y1="12" x2="6" y2="12" />
+            <line x1="18" y1="12" x2="22" y2="12" />
+          </svg>
         </div>
       </span>
     })()}
